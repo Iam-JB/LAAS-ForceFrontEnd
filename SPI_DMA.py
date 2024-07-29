62c32f09b0fd0e4041a934ab51cd1b2e1210b53b
Iam-JB <116198831+Iam-JB@users.noreply.github.com>
2024-07-29 14:46:11 +0200

    RPi - Add of ADC internal temperature calculation

--- SPI_DMA.py
+++ SPI_DMA.py
@@ -3,10 +3,10 @@
 #Jean-Baptiste CAZAUX - Tuesday 16 July
 #https://github.com/Iam-JB/LAAS-ForceFrontEnd
 
-print ("Hello Gepetto Team !")
+print ("Hello Gepetto Team !\n")
 
 # Run program as root in order to access all peripherals (sudo /usr/bin/thonny %F)
-
+ 
 ############################################################################################
 #                                                                                          #
 #                                      IMPORT / EXPORT                                     #
@@ -50,21 +50,14 @@ nRESET = 26
 
 # GPIOs and SPI INITIALISATION
 
-def initRaspPI():
+def initRPi():
    GPIO.setmode(GPIO.BCM)
+   
    GPIO.setup(nCS,GPIO.OUT)
-#   GPIO.setup(MOSI,GPIO.OUT)			# don't need to define those pins as gpio as there are used for SPI communication
-#   GPIO.setup(MISO,GPIO.IN)
-#   GPIO.setup(SCLK,GPIO.OUT)
    GPIO.setup(START,GPIO.OUT)
    GPIO.setup(nRESET,GPIO.OUT)
    GPIO.setup(nPWDN,GPIO.OUT)
    GPIO.setup(nDRDY,GPIO.IN)
-
-   GPIO.add_event_detect(nDRDY,
-                         GPIO.FALLING,
-		                 callback=myCallback)
-                         # bouncetime = ?
 
    if not bcm2835_init():
        print ("BCM2835 : Initialisation failed")
@@ -73,10 +66,10 @@ def initRaspPI():
 
    bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST)
    bcm2835_spi_setDataMode(BCM2835_SPI_MODE1)
-   bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_256)
-   bcm2835_spi_chipSelect(BCM2835_SPI_CS_NONE)
+   bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_256) # CLK AT 1MHz
+#   bcm2835_spi_chipSelect(BCM2835_SPI_CS_NONE)
 
-   print ("Initialisation has been successfully done !")
+#   print ("Initialisation has been successfully done !")
 
 # READ ADS1235Q1's REGISTERS
 
@@ -98,12 +91,12 @@ def writeRegister(address,value):
    bcm2835_spi_transfer(value)
    GPIO.output(nCS,GPIO.HIGH)
 
-def setup():
+def setupMain():
 
-   print ("START OF SETUP TASK")
+#   print ("START OF MAIN SETUP TASK")
 
    # INIT
-   initRaspPI()
+   initRPi()
 
    GPIO.output(nCS,GPIO.HIGH)
    GPIO.output(nRESET,GPIO.HIGH)
@@ -111,27 +104,11 @@ def setup():
 
    # DATA RATE CONFIGURATION
    addressDR = 0b00010
-#   valueDR = readRegister(addressDR)
    valueDR = 0b01100100			# 7200 SPS
    writeRegister(addressDR,valueDR)
 
-   readRegister(addressDR)
-
-   # PGA CONFIURATION
-   addressPGA = 0b10000
-#   valuePGA = readRegister(addressPGA)
-   valuePGA = 0b10000111 		# Gain of 128
-   writeRegister(addressPGA,valuePGA)
-
-   # INPUT MULTIPLEXER CONFIGURATION
-   addressMUX = 0b10001
-#   valueMUX = readRegister(addressMUX)
-   valueMUX = 0b01111000		# AIN4 and AIN5
-   writeRegister(addressMUX,valueMUX)
-
    # REFERENCE CONFIGURAION
    addressREF = 0b00110
-#   valueREF = readRegister(addressREF)
    valueREF = 0b00001010
    writeRegister(addressREF,valueREF)
 
@@ -144,8 +121,55 @@ def setup():
    writeRegister(0x0B,0x00)
    writeRegister(0x0C,0x40)
 
-   print("setup is done !")
+#   print("SETUP IS DONE !")
+
+def setupLoadCells():
+    
+   # PGA CONFIURATION
+   addressPGA = 0b10000
+   valuePGA = 0b10000111 		# Gain of 128
+   writeRegister(addressPGA,valuePGA)
+
+   # INPUT MULTIPLEXER CONFIGURATION
+   addressMUX = 0b10001
+   valueMUX = 0b01111000		# AIN4(+) and AIN5(-)
+   writeRegister(addressMUX,valueMUX)
+
+# Internal temperature measurement here once and then in parallel of the adc value (priority goes to adc value)
+def getTemperature():
+    
+   # PGA Gain = 1
+   addressPGA = 0b10000
+   valuePGA = 0b00000000 		# Gain of 1
+   writeRegister(addressPGA,valuePGA)
    
+   # INMUX = 0b10111011
+   addressMUX = 0b10001
+   valueMUX = 0b10111011		# Internal Temperature Sensor
+   writeRegister(addressMUX,valueMUX)
+   
+   tmp = 0
+   Temperature = 0
+   TemperatureCelsius = 0
+# GPIO.output(START,GPIO.HIGH)
+# while nDRDY:
+#    print("waiting...")
+#    time.sleep(0)
+
+   GPIO.output(nCS,GPIO.LOW)
+   bcm2835_spi_transfer(0x12)
+   bcm2835_spi_transfer(0xFF)
+   MSB = bcm2835_spi_transfer(0x00)
+   MIDSB = bcm2835_spi_transfer(0x00)
+   LSB = bcm2835_spi_transfer(0x00)
+   GPIO.output(nCS,GPIO.HIGH)
+
+   dataTemp = [MSB,MIDSB,LSB]
+   for byteTemp in dataTemp:
+      Temperature = (tmp << 8) | byteTemp
+   TemperatureCelsius = ((Temperature - 122.4) / 420) + 25  # Temperature (°C) = [(Temperature Reading (uV) - 122.4) / 420 uV/°C] + 25°C
+   print("ADC Internal Temperature : ",TemperatureCelsius,"°C")
+    
 ############################################################################################
 #                                                                                          #
 #                                           IT                                             #
@@ -154,8 +178,6 @@ def setup():
 
 # IT triggered on nDRDY has been configured through 'add_event_detect' in initRaspPI() function
 
-global targetTimeSPI
-targetTimeSPI = 0.000138
 adcTab = []
 spiTimeTab = []
 
@@ -171,16 +193,11 @@ def myNoiseRMS(noiseTab):
     return noiseRMS
 
 def myCallback(channel):
-#   print ("ENTERING THE CALLBACK")
-   
-#   global ADC_VALUE
-#   targetTimeSPI = 0.000138   # T = 1/7200
-
    GPIO.output(START,GPIO.LOW)
    GPIO.output(nCS,GPIO.LOW)
 
-#   print ("Data is now ready !")
    ADC_VALUE = 0
+   ADC_VALUE_CAL = 0
 
    startTimeSPI = time.time()
    
@@ -192,6 +209,15 @@ def myCallback(channel):
    
    stopTimeSPI = time.time()
    
+   realTimeSPI = stopTimeSPI - startTimeSPI
+   spiTimeTab.append(realTimeSPI)
+   if (len(spiTimeTab)>= 5000):
+      spiTimeMean = np.mean(spiTimeTab)*1000000  # useconds
+      # Sending SPI Mean Time to PlotJuggler
+      messageSPI = {"SPI Time (us)" : spiTimeMean}
+      socket.send_string(json.dumps(messageSPI))
+      spiTimeTab.clear()
+   
    data = [MSB,MIDSB,LSB]
    
    for byte in data:
@@ -199,49 +225,31 @@ def myCallback(channel):
 
    # CALIBRATION : 100g i.e 0.981N corresponds to an adc code of 3150
    if (ADC_VALUE >= 10000000 and ADC_VALUE <= 16778000):
-       ADC_VALUE = (ADC_VALUE - 16747800)*0.981/3150
+       ADC_VALUE_CAL = (ADC_VALUE - 16747800)*0.981/3150
    else :
-       ADC_VALUE = (ADC_VALUE + 29000)*0.981/3150
-   # @TODO when the 'normalized' adc value arrives at 30000 it crashes down to approx. -16746500
-   #       where does that come from ??
+       ADC_VALUE_CAL = (ADC_VALUE + 29500)*0.981/3150
 
-   realTimeSPI = stopTimeSPI - startTimeSPI
-   spiTimeTab.append(realTimeSPI)
-   if (len(spiTimeTab)>= 5000):
-      spiTimeMean = np.mean(spiTimeTab)*1000000
-      # Sending SPI Mean Time to PlotJuggler
-      messageSPI = {"SPI Time (us) " : spiTimeMean}
-      socket.send_string(json.dumps(messageSPI))
-      spiTimeTab.clear()
-
-   adcTab.append(ADC_VALUE)
+#    if (ADC_VALUE < -2 or ADC_VALUE > 100):
+#       raise (Exception("ERR : The ADC Value is out of normal range. Please reset the ADS1235Q1."))
+#       time.sleep(1)
+      
+   adcTab.append(ADC_VALUE_CAL)
    if (len(adcTab)>=5000):              # Calculating the noise every 5000 samples, is that enough ?
        noiseTab = zeros(len(adcTab),float)
        adcMean = np.mean(adcTab)
-#       print("adc tab : ",adcTab)
+
        for index in range(0,len(adcTab)-1):
           noiseTab[index] = abs(adcTab[index] - adcMean)
           
        noiseRMS = myNoiseRMS(noiseTab)
        # Sending Noise RMS to PlotJuggler
-       messageRMS = {"NOISE RMS " : noiseRMS}
+       messageRMS = {"NOISE RMS (N)" : noiseRMS}
        socket.send_string(json.dumps(messageRMS))
    
        adcTab.clear()
-#       time.sleep(10)
-    
-#    if (realTimeSPI <= targetTimeSPI):
-#       print("The SPI conversion time is respected")
-#       return
-#    if (realTimeSPI > targetTimeSPI):
-#       print("The SPI conversion time isn't respected")
-#    print(realTimeSPI)
-
-#   print(ADC_VALUE)   # Not usefull here as it is sent to PlotJuggler for better high speed data vialization
-                       # AND IT IS GOING TO SLOW DOWN PLOTJUGGLER VISUALISATION
     
    # Sending ADC data to PlotJuggler
-   messageADC = {"ADC VALUE " : ADC_VALUE}
+   messageADC = {"ADC VALUE (N)" : ADC_VALUE_CAL}
    socket.send_string(json.dumps(messageADC))
 
    GPIO.output(nCS,GPIO.HIGH)
@@ -255,20 +263,33 @@ def myCallback(channel):
 #                                                                                          #
 ############################################################################################
 
-print ("START OF THE MAIN TASK")
+#print ("START OF THE MAIN FUNCTION")
+
+print("Please visualize all data in Plot Juggler with the following ZMQ SUB configuration :\n")
+print("   Transport     Address     Port   |  Message Protocol")
+print("    tcp://      localhost    9872   |        json    \n")
 
 GPIO.setwarnings(False)
 
-setup() 
-GPIO.output(START,GPIO.HIGH)        # start the first conversion
+setupMain()
+
+getTemperature()
+
+setupLoadCells()
+
+GPIO.add_event_detect(nDRDY,
+                      GPIO.FALLING,
+                      callback=myCallback)
+                    # bouncetime = ?
+
+GPIO.output(START,GPIO.HIGH) # start first conversion
 
 try :
    while True:
       # Nothing here as it is handled by IT
       time.sleep(0)
-         
-except KeyboardInterrupt:
+
+except KeyboardInterrupt: 
    bcm2835_spi_end()
    bcm2835_close()
    GPIO.cleanup()
-   
